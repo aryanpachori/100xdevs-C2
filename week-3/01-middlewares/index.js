246a795f82938013244930c50826722aad36e911
Aryan Pachori <aryanpachori03@gmail.com>
2024-02-08 18:01:15 +0530

    middlewares

---
+++ week-3/01-middlewares/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+app.use(express.json());
+
+mongoose.connect(
+  "mongodb+srv://aryanpachori03:asWReakPkuZ80d89@aryan.k9vagyu.mongodb.net/"
+);
+
+const User = mongoose.model('Users', {
+  name: String,
+  email: String,
+  password: String,
+});
+
+app.post("/signup", async function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+  const name = req.body.name;
+
+  const exisitngUser = await User.findOne({ email: username });
+  if (exisitngUser) {
+    return res.status(400).send("Email already registered");
+  }
+  const user = new User({
+    name: name,
+    email: username,
+    password: password,
+  });
+  user.save();
+  res.json({
+    msg: "User created successfully ",
+  });
+});
+app.listen(3000);
