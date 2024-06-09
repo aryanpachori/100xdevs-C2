9eef08fac14447b77d5a527835c53a86fd658e78
Aryan Pachori <aryanpachori03@gmail.com>
2024-06-09 07:09:34 +0530

    cookies

---
+++ week-16-auth/backend/src/index.ts
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "Myjwtsecret";
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+app.use(express.json());
+app.use(cookieParser);
+app.use(
+  cors({
+    Credential: true,
+    origin: "http://localhost:5173/",
+  })
+);
+// @ts-ignore
+app.post("/signin", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const token = jwt.sign({ id: 1 }, JWT_SECRET);
+  res.cookies("token", token);
+  res.send("Logged in!");
+});
+// @ts-ignore
+app.get("/user", (req, res) => {
+  const token = req.cookies.token;
+  const decoded = jwt.verify(token, JWT_SECRET);
+  res.send({
+    userId: decoded.id,
+  });
+});
+// @ts-ignore
+app.post("/logout", (req, res) => {
+  res.cookie("token", "ads");
+  res.json({
+    message: "Logged out!",
+  });
+});
+
+app.listen(3000);
