af5f194fe75731d6dc7f9e763b8fc0a5e22b20b6
Aryan Pachori <aryanpachori03@gmail.com>
2024-07-24 22:56:11 +0530

    client

--- week-19-ws/src/index.ts
+++ week-19-ws/src/index.ts
@@ -1,17 +1,25 @@
 import http from "http";
-import { WebSocketServer } from "ws";
+import { WebSocketServer ,WebSocket } from "ws";
 
 const server = http.createServer(function (request, response) {
   console.log(new Date() + "Received req for" + request.url);
   response.end("hi there");
 });
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", function connection(socket) {
+  socket.on("error", console.error);
+
+  socket.on("message", function message(data,isBinary) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState == WebSocket.OPEN) {
+        client.send(data,{binary: isBinary});
+      }
+    });
+  });
+  socket.send('Hello! Message From Server!!');
+});
+
 server.listen(8080, function () {
   console.log(new Date() + "server running");
 });
-
-const wss = new WebSocketServer({ server });
-
-wss.on('connection' ,function connection(ws){
-     ws.on('error',console.error);
-     
-})
