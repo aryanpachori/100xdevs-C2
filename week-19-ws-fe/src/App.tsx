43a875179b091e22e6d65e1c19edeeb43f1f1f37
Aryan Pachori <aryanpachori03@gmail.com>
2024-07-25 00:30:09 +0530

    socket config

--- week-19-ws-fe/src/App.tsx
+++ week-19-ws-fe/src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [socket, setSocket] = useState<null | WebSocket>(null);
+  const [message, setMessage] = useState("");
+  const [value, setValue] = useState("");
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:8080/");
+    socket.onopen = () => {
+      console.log("connected");
+      setSocket(socket);
+    };
+    socket.onmessage = (message) => {
+      setMessage(message.data);
+    };
+    setSocket(socket);
+  }, []);
+  if (!socket) {
+    return <div>connecting to WebSocket...</div>;
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <input
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      ></input>
+      <br/>
+      <button
+        onClick={() => {
+          socket.send(value);
+        }}
+      >
+        send
+      </button>
+      <br/>
+      {message}
+    </div>
+  );
 }
 
-export default App
+export default App;
